9dd1c270c03e6f27b8f977251f495b3f823f9c6d
ApliCalvin <thecalvins.sims@gmail.com>
2025-04-24 09:20:33 +0700

    create structure folder

---
+++ character/Character.java
@@ -0,0 +1,13 @@
+package uts.character;
+
+class Character {
+    protected int HP, attack, defense;
+    
+    void viewHP() {
+        System.out.println("HP anda : " + this.hp);
+    }
+
+    void attact() {
+        
+    }
+}
---
+++ enemy/Enemy.java
@@ -0,0 +1,7 @@
+package uts.enemy;
+import uts.character.Character;
+
+class Enemy extend Character {
+    HP = 150;
+    
+}
